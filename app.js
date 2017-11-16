b4fa7117c153572ae2ee9901c45ef50ddc2191a9
Alex Wong <alexw14@gmail.com>
2017-11-15 17:41:25 -0800

    Created config, controllers, and models dir

--- app.js
+++ app.js
@@ -4,16 +4,20 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var methodOverride = require('method-override'); // require method override
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
 
+require('./config/db'); // require the database
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(methodOverride("_method")); // mount method override
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
