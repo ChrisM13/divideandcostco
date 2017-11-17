eea93b1f43666f1a7f26faf8ca0e9681bd9a433b
Alex Wong <alexw14@gmail.com>
2017-11-17 12:04:12 -0800

    added user and product model

--- models/user.js
+++ models/user.js
@@ -1,2 +1,32 @@
 var mongoose = require('mongoose');
 
+var listSchema = new mongoose.Schema(
+    {
+        products: [{type: mongoose.Schema.Types.ObjectId, ref: 'Product'}],
+        zipCode: Number,
+        isActive: {type: Boolean, default: true}
+    },
+    {
+        timestamps: true
+    }
+);
+
+var userSchema = new mongoose.Schema(
+    {
+        name: String,
+        email: String,
+        avatar: String,
+        facebookId: String,
+        lists: [listSchema]
+    },
+    {
+        timestamps: true
+    }
+);
+
+// Get current user list
+userSchema.methods.currentList(function(){
+    return this.lists.find(list => list.isActive);
+});
+
+module.exports = mongoose.model('User', userSchema);
