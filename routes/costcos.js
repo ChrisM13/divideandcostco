ece441138f14667cfd418d3790465d9f2846bb84
Dylmarcor <dylancorbett@me.com>
2017-11-18 14:35:28 -0800

    Added CostcosController and routes

---
+++ routes/costcos.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+var yelpCtrl = require('./../controllers/api/yelpController');
+var costcosCtrl = require('./../controllers/costcosController');
+var request = require('request')
+
+router.get('/', (req, res) => {
+    res.render('costcos/index');
+});
+
+module.exports = router;    
