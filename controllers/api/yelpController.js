8586568c39fe674de88f0c9052011bbde49273ba
Chris Mosier <chrismosier13@gmail.com>
2017-11-17 16:53:24 -0800

    added limit to 1 on findCostco function

--- controllers/api/yelpController.js
+++ controllers/api/yelpController.js
@@ -5,7 +5,7 @@ var rootURL = 'https://api.yelp.com/v3/businesses/search';
 
 function findCostco(req, res, next) {
     var options = {
-        url: `${rootURL}?term=costco&location=${req.body.zipCode}`,
+        url: `${rootURL}?term=costco&location=${req.body.zipCode}&limit=1`,
         headers: {
             'Authorization': `Bearer ${process.env.YELP_TOKEN}`
         }
@@ -13,7 +13,7 @@ function findCostco(req, res, next) {
     request(options, function(err, response, body) {
         var costcoData = JSON.parse(body);
         console.log('made it to Costco')
-        res.render('index', { user: req.user, costcoData });
+        res.render('index', { user: req.user, costcoData: response });
         console.log('this is costcoData', costcoData)
     });
 }
