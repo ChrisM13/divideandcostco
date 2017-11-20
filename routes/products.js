a087a87461ce21d0ed36210f476fc5650018696f
Dylmarcor <dylancorbett@me.com>
2017-11-20 10:32:30 -0800

    Fixed product model

--- routes/products.js
+++ routes/products.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var product = require('./../models/product');
+var productCtrl = require('./../controllers/productsController');
 
 router.get('/', (req, res) => {
     console.log(res);
-    res.render('/views/products');
+    res.render('products');
 });
 
+module.exports = router;
