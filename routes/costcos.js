df870004ed97bd8783e6df52c51997a47e370012
Alex Wong <alexw14@gmail.com>
2017-11-19 23:16:30 -0800

    added google map api

--- routes/costcos.js
+++ routes/costcos.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var router = express.Router();
-var yelpCtrl = require('./../controllers/api/yelpController');
-var costcosCtrl = require('./../controllers/costcosController');
-var request = require('request')
+// var yelpCtrl = require('./../controllers/api/yelpController');
+// var costcosCtrl = require('./../controllers/costcosController');
+// var request = require('request')
 
 router.get('/', (req, res) => {
     res.render('costcos/index');
