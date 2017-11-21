f76c4f2a248872c03b76fec3785f79be6534d792
Chris Mosier <chrismosier13@gmail.com>
2017-11-21 10:06:25 -0800

    removed prodectsController

--- controllers/costcosController.js
+++ controllers/costcosController.js
@@ -4,7 +4,7 @@ var User = require('../models/user');
 function show(req, res) {
     console.log('cats');
     console.log(req.params);
-    User.find({'lists.zipCode': req.params.zip, isActive: true}, (err, users) => {
+    User.find({'lists.zipCode': req.params.zip, 'lists.isActive': true}, (err, users) => {
         console.log(users);
         var lists = users.map(user => ({
             name: user.name,
