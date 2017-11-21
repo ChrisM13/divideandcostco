b9e68ade82fc894a3ac30c459ec1198b3534e309
Alex Wong <alexw14@gmail.com>
2017-11-21 10:00:56 -0800

    removed product routes and ctrl

--- controllers/listController.js
+++ controllers/listController.js
@@ -1,10 +1,26 @@
 var User = require('./../models/user');
+var Product = require('./../models/product');
 
+function show(req, res) {
+    Product.find({}, (err, products) => {
+        res.render('lists/show', {user: req.user, products});
+    });
+};
 
 function createList(req, res) {
-    req.User.lists.create({products: req.})
-}
+    var list = req.user.currentList();
+    if (!list) {
+        req.user.lists.push({zipCode: req.body.zipCode});
+        list = req.user.currentList();
+    }
+    list.products = req.body.products;
+    req.user.save();
+    Product.find({}, (err, products) => {
+        res.render('lists/show', {user: req.user, products});
+    });
+};
 
 module.exports = {
+    show,
     createList
 };
--- routes/costcos.js
+++ routes/costcos.js
@@ -2,13 +2,10 @@ var express = require('express');
 var router = express.Router();
 // var yelpCtrl = require('./../controllers/api/yelpController');
 var costcosCtrl = require('./../controllers/costcosController');
-// var request = require('request')
 
-router.get('/', (req, res) => {
-    res.render('costcos/index');
-});
+
+router.get('/', costcosCtrl.index); 
 
 router.get('/:zip', costcosCtrl.show);
     
-
 module.exports = router;    
