480bdfbee1df9c7ea39321b68e1abd0989dbdcc5
Chris Mosier <chrismosier13@gmail.com>
2017-11-27 09:24:04 -0800

    prepare to add item images

--- routes/costcos.js
+++ routes/costcos.js
@@ -3,15 +3,8 @@ var router = express.Router();
 var log = require('./../util/auth');
 var costcosCtrl = require('./../controllers/costcosController');
 
-<<<<<<< HEAD
-router.get('/:zip', costcosCtrl.show);
-
-router.post('/', costcosCtrl.index);
-
-=======
 router.get('/:zip', log.isLoggedIn, costcosCtrl.show);
 router.get('/:zip/connection', log.isLoggedIn, costcosCtrl.connection);
 router.post('/', log.isLoggedIn, costcosCtrl.index);
     
->>>>>>> master
 module.exports = router;    
